d07fedc712efa750a3b8771388df05b02d3d0d75
thanhnh150500 <thanhnh150500@gmail.com>
2022-08-07 12:07:12 +0700

    Update PnpList.ps1

--- PnpList.ps1
+++ PnpList.ps1
@@ -7,16 +7,44 @@ $CredInfo = New-Object System.Management.Automation.PSCredential($SiteUser, $Pas
 			Write-Host "Connecting... to site:"$SiteUrl -f Green
 			Connect-PnPOnline $SiteUrl -Credentials $CredInfo -WarningAction Ignore
 			Write-Host "Connected" -f Green
+# Function check if List Exists
+function CheckExistsField($FieldCheck , $ListCheck){
+    Write-Host $FieldCheck -f Blue
+    $FieldName = Get-PnPField -List $ListCheck -Identity $FieldCheck
+    Write-Host $FieldName.Title 
+    if($FieldName.Title -eq $FieldCheck){
+        Write-Host $FieldCheck " exists" -f Blue
+    }
+    else{
+        Write-Host $FieldCheck " not exists" -f Red
+    }
+}
+
+try {
+    $ListName = "Lst_Pnp_Demo"		
+    $List = Get-PnPList -Identity $ListName
+    
+    If ($List.Title -eq $ListName) {  
+       # Write-Host $listTitle " - List exist!"
+       $Vari = "PnpLocation"
+       CheckExistsField -FieldCheck $Vari -ListCheck $ListName 
+    }
+
+    ELSE {
+        #Create a List name demo
+        New-PnPList -Title $ListName -Template GenericList
 			
-$ListName = "PnpRegistrationDetails"			
+        #Create a column name Location of Text
+        Add-PnPField -List $ListName -DisplayName "PnpLocation" -InternalName "PnpLocation" -Type Text
 			
-            #Create a List name demo
-			New-PnPList -Title $ListName -Template GenericList
-			
-			#Create a column name Location of Text
-			Add-PnPField -List $ListName -DisplayName "PnpLocation" -InternalName "PnpLocation" -Type Text
-			
-			$FieldXml = '<Field Type="Text" DisplayName="Pnp Demo Name" Required="TRUE" MaxLength="255" Name="PnpDemoName" Description="Pnp Name for the Demo" />'
-			Add-PnPFieldFromXml -FieldXml $FieldXml -List $ListName
-			
-            Write-Host "Done" -f Green
+        $FieldXml = '<Field Type="Text" DisplayName="Pnp Demo Name" Required="TRUE" MaxLength="255" Name="PnpDemoName" Description="Pnp Name for the Demo" />'
+        Add-PnPFieldFromXml -FieldXml $FieldXml -List $ListName
+		CheckExistsField("PnpLocation",$ListName)	
+        Write-Host "Done" -f Green
+       # Write-Host $listTitle " - List not exist!"  
+    }
+}
+catch {
+    Write-Host"erorr!!!" -f Blue
+    #{ 1:<#Do this if a terminating exception happens#> }
+}
